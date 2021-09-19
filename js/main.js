ad93e36e492d0e176ff8640cfcb7208265297f31
Marina Zabrodska <marinazabrodska8@gmail.com>
2021-09-19 22:28:45 +0300

    finished footer html/css and add adaptive to header

--- js/main.js
+++ js/main.js
@@ -3,4 +3,7 @@ $(function(){
         prevArrow: '<img class="slider__arrow slider__arrow-left" src="/images/Arrow left.svg" alt="arrow_left">',
         nextArrow: '<img class="slider__arrow slider__arrow-right" src="/images/Arrow right.svg" alt="arrow_right">'
     });
+    $('.menu-btn').on('click', function(){
+        $('.menu__list').toggleClass('active');
+    });
 });
